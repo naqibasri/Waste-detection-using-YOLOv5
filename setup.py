a60921e1ab7fda4cca8425a427f564a80a770801
naqib <naqib.asri.294@gmail.com>
2023-10-14 10:46:00 +0800

    requirement & venv completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'naqibasri',
+    author_email= 'naqib.asri.294@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
